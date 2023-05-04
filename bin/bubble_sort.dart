b7390366df54904331bcf9a05b603f55836fbb19
unknown <akbromz@gmail.com>
2023-05-04 21:55:29 +0530

    insertion sort

--- bin/bubble_sort.dart
+++ bin/bubble_sort.dart
@@ -1,6 +1,19 @@
-List<int> bubbleSort(List<int> list) {
+// List<int> bubbleSort(List<int> list) {
+//   for (int i = 0; i < list.length; i++) {
+//     for (int j = 0; j < list.length - i - 1; j++) {
+//       if (list[j] > list[j + 1]) {
+//         int temp = list[j];
+//         list[j] = list[j + 1];
+//         list[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return list;
+// }
+
+bubbleSort(List<int> list) {
   for (int i = 0; i < list.length; i++) {
-    for (int j = 0; j < list.length - i - 1; j++) {
+    for (int j = 0; j < list.length - 1; j++) {
       if (list[j] > list[j + 1]) {
         int temp = list[j];
         list[j] = list[j + 1];
--- bin/insertion_sort.dart
+++ bin/insertion_sort.dart
@@ -1,15 +1,3 @@
-// insertionSort(List<int> list) {
-//   for (int i = 1; i < list.length; i++) {
-//     int temp = list[i];
-//     int j = i - 1;
-//     while (j >= 0 && list[j] > temp) {
-//       list[j + 1] = list[j];
-//       j--;
-//     }
-//     list[j + 1] = temp;
-//   }
-//   return list;
-// }
 insertionSort(List<int> list) {
   for (int i = 1; i < list.length; i++) {
     int temp = list[i];
@@ -22,3 +10,15 @@ insertionSort(List<int> list) {
   }
   return list;
 }
+// insertionSort(var list) {
+//   for (int i = 1; i < list.length; i++) {
+//     int temp = list[i];
+//     int j = i - 1;
+//     while (list[j] > temp && j >= 0) {
+//       list[j + 1] = list[j];
+//       j--;
+//     }
+//     list[j + 1] = temp;
+//   }
+//   return list;
+// }
